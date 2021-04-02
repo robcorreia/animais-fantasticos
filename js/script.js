7f75ad6841c3271c5b3cc0c85f2be6adc613c876
Robson <robccorreia@gmail.com>
2021-04-02 17:45:08 -0300

    ajuste

--- js/script.js
+++ js/script.js
@@ -36,7 +36,6 @@ const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
 menuMobile.init();
 
 const funcionamento = new Funcionamento('[data-semana]', 'aberto');
-console.log(funcionamento);
 funcionamento.init();
 
 
